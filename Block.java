e4f9dd7df86a38ea378e285565345533ee31d7e6
Sharko <35842212+Oshaik@users.noreply.github.com>
2020-12-03 20:46:38 -0500

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,1 +1,21 @@
-
+public class Block{
+	private String data;
+	private String hash;
+	private String prevHash;
+	
+	public Block(String d){
+		this.data = d;
+	}
+	
+	public String getData(){
+		return this.data;
+	}
+	
+	public String getHash(){
+		return this.hash;
+	}
+	
+	public String getPrevHash(){
+		return this.prevHash;
+	}
+}
