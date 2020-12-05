958c2bc9403e8c450a8a41c13864fc586b6892af
Sharko <35842212+Omar-Shaik@users.noreply.github.com>
2020-12-05 09:56:09 -0500

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,1 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+public interface Interface extends Remote {
+   public byte[] downloadFile(String fileName) throws
+   RemoteException;
+}
