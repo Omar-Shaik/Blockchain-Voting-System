ea636acaa110d4d3c0cad7750a704b2e5516812c
Sharko <35842212+Omar-Shaik@users.noreply.github.com>
2020-12-05 10:00:21 -0500

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -2,6 +2,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Interface extends Remote {
-   public boolean vote(String sin) throws
+   public boolean vote(String driversID) throws
    RemoteException;
 }
