f9ce8551b3da13e94e8f906d94dd97a73114c376
Sohaib Mohiuddin <sohaib.mohiuddin@ontariotechu.net>
2020-12-07 10:33:58 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,10 +5,10 @@ import java.rmi.registry.Registry;
 import java.util.ArrayList;
 
 public class Client {
+    private static String fromKiosk;
+    private static Interface fi;
 
     public static void main(String argv[]) {
-        // Console is used for user input
-        Console console = System.console();
 
         String machine_name = argv[0];
 		if (argv.length != 2) {
@@ -17,20 +17,17 @@ public class Client {
         }
         
         try {
-            //String name = "BlockServer";
             Registry registry = LocateRegistry.getRegistry(machine_name);
-            Interface fi = (Interface) registry.lookup(argv[1]);
-            String driversID;
-
-            driversID = console.readLine("Driver's License #: ");
+            fi = (Interface) registry.lookup(argv[1]);            
             
-            boolean voted = fi.vote(driversID);
-
-            System.out.println(voted);
         } catch (Exception e) {
-            //System.out.println("Client err: " + e.getMessage());
             e.printStackTrace();
         }
     }
+    public static void setUIInfo(String licenseCandidate) throws Exception {
+        fromKiosk = licenseCandidate;
+        boolean voted = fi.vote(fromKiosk);
 
+        System.out.println(voted);
+    }
 }
